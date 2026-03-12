f0ff7801448921d9ffe0d275b757892e9bc6669a
HenriqueSena135 <riqueboy2006@hotmail.com>
2026-03-11 21:53:45 -0300

    Aula12

---
+++ Lógica de Programação e Algoritmos/Aula12/tupla.cs
@@ -0,0 +1,10 @@
+//tupla
+var clodoaldo = (34, 34, "Heitor", "SENAI", 'B');
+
+System.Console.WriteLine($"Idade do Clodoaldo: {clodoaldo.Item1}");
+System.Console.WriteLine($"idade do Clodoaldo : {clodoaldo.Item2}");
+System.Console.WriteLine($"Nome do Filho do Clodo: {clodoaldo.Item3}");
+clodoaldo.Item1 = 54;
+System.Console.WriteLine($"Idade de Clodoaldo:{clodoaldo.Item1}");
+(int, int,int)numeros = (1,2,5);
+System.Console.WriteLine($"Numeros: {Numeros.Item1},{Numeros.Item2},{numeros.Item5}");
