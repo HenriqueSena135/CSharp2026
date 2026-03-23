0a7bd9945cf001cf86fb663de455d43edf564de9
HenriqueSena135 <riqueboy2006@hotmail.com>
2026-03-23 19:12:36 -0300

    Aula03

---
+++ POO/Aula 03/App1/App1/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace App1
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double Salario_Bruto;
+        public double Imposto;
+
+        public Funcionario(string Nome, double Salario_Bruto, double Imposto)
+        {
+            this.Nome = Nome;
+            this.Salario_Bruto = Salario_Bruto;
+            this.Imposto = Imposto;
+        }
+
+        public double Salario_Liquido()
+        {
+            return Salario_Bruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            double salarioLiquido = Salario_Liquido();
+            double aumento = salarioLiquido * porcentagem / 100.0;
+            double novoLiquido = salarioLiquido + aumento;
+
+            Salario_Bruto = novoLiquido + Imposto;
+        }
+    }
+}
+
+
---
+++ POO/Aula 03/App1/App1/Program.cs
@@ -0,0 +1,30 @@
+﻿using App1;
+using static System.Console;
+
+WriteLine("Entre com os dados do funcionario:");
+
+Write("Digite o nome do funcionario: ");
+string nome = ReadLine();
+
+Write("Digite o salário bruto do funcionario: ");
+double salarioBruto = double.Parse(ReadLine());
+
+Write("Digite o valor total de imposto pago pelo funcionario: ");
+double imposto = double.Parse(ReadLine());
+
+Funcionario f = new Funcionario(nome, salarioBruto, imposto);
+
+WriteLine("\nDados do Funcionario:");
+WriteLine($"Nome: {f.Nome}");
+WriteLine($"Salario Liquido: {f.Salario_Liquido()}");
+
+Write("\nDigite a porcentagem para aumentar o salário: ");
+double porcentagem = double.Parse(ReadLine());
+
+// Aplicando aumento
+f.AumentarSalario(porcentagem);
+
+WriteLine("\nDados atualizados:");
+WriteLine($"Nome: {f.Nome}");
+WriteLine($"Salario Liquido: {f.Salario_Liquido()}");
+ReadKey();
