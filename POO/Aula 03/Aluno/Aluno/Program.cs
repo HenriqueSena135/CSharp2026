c28fac896efa5208d24966d08179fc4bd8f3951d
HenriqueSena135 <riqueboy2006@hotmail.com>
2026-03-23 19:38:34 -0300

    Aluno

---
+++ POO/Aula 03/Aluno/Aluno/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.NomeDoAluno = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno:");
+
+            aluno.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Nota final do aluno: "
+                + aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (aluno.AprovadoOuReprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("Faltaram "
+                    + aluno.Ponto().ToString("F2", CultureInfo.InvariantCulture)
+                    + " pontos");
+            }
+        }
+    }
+}
---
+++ POO/Aula 03/Aluno/Aluno/nota.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ProjetoAluno
+{
+    class Aluno
+    {
+        public string NomeDoAluno;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public bool AprovadoOuReprovado()
+        {
+            return NotaFinal() >= 60.0;
+        }
+
+        public double Ponto()
+        {
+            if (AprovadoOuReprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+    }
+}
