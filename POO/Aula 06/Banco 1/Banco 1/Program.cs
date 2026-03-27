1eb661e963d0b7387ce12a16d257b28fba60e10c
HenriqueSena135 <riqueboy2006@hotmail.com>
2026-03-27 20:54:18 -0300

    Aula Banco 1

---
+++ POO/Aula 06/Banco 1/Banco 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Banco_1;
+using static System.Console;
+
+Title = "Cadastro de conta";
+ContaBancaria conta;
+
+//Entrada de dados
+Write("Entre com o Número da conta:");
+int n = int.Parse(Console.ReadLine());
+Write("Entre com o Nome do titular da conta:");
+string nome = ReadLine();
+Write("Deseja Fazer depósito inicial (s/n):");
+char resposta = char.Parse(ReadLine().ToLower());
+if (resposta == 's')
+{
+    Write("Entre com Valor do Depósito inicial:");
+    double dep = double.Parse(Console.ReadLine());
+    conta = new ContaBancaria(n, nome, dep);
+}
+else
+{
+    conta = new(n, nome);
+}
+conta.Dados();
+ReadKey();
